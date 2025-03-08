3c4338b0b51f3c7985ed7cccbd4c0c15b64f6e49
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2025-03-08 20:12:40 +0100

    Update process script

--- grobid/README.md
+++ grobid/README.md
@@ -6,3 +6,23 @@ python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 ```
+
+### Test if gpu is available
+```bash
+docker run --rm -it --gpus=all nvcr.io/nvidia/k8s/cuda-sample:nbody nbody -gpu -benchmark
+```
+
+- [Full model](https://kermitt2-grobid.hf.space/)
+- [Lite model](https://kermitt2-grobid-crf.hf.space/)
+- [GROBID docker documentation](https://grobid.readthedocs.io/en/latest/Run-Grobid/)
+
+### Increase wsl2 memory
+```bash
+wsl --shutdown
+```
+Open `C:\Users\username\.wslconfig` and add the following:
+```bash
+[wsl2]
+memory=12GB
+swap=25GB
+```
--- grobid/processPDF.py
+++ grobid/processPDF.py
@@ -1,12 +1,23 @@
 import os
+
 from grobid_client.grobid_client import GrobidClient
 
-input_dir = 'input'
-output_dir = 'output'
+input_dir = "input"
+output_dir = "output"
 os.makedirs(input_dir, exist_ok=True)
 os.makedirs(output_dir, exist_ok=True)
 
 client = GrobidClient(config_path="config.json")
+
+print(f"Removing leftover files in folder {output_dir} ...")
+for file in os.listdir(output_dir):
+    if file.endswith(".txt"):
+        os.remove(os.path.join(output_dir, file))
+
+print(f"Add .pdf extension to all files in folder {input_dir} ...")
+for file in os.listdir(input_dir):
+    if not (file.endswith(".pdf") or file.endswith(".json")):
+        os.rename(os.path.join(input_dir, file), os.path.join(input_dir, file + ".pdf"))
 
 print(f"Processing all PDFs in folder {input_dir} ...")
 client.process("processFulltextDocument",
@@ -16,3 +27,12 @@ client.process("processFulltextDocument",
                verbose=True
                )
 print(f"TEI XML files saved in {output_dir}")
+
+print(f"List of files not processed:")
+for file in os.listdir(output_dir):
+    if file.endswith(".txt"):
+        bad_file = file.split("_")[0] + ".pdf"
+        os.makedirs("bad_files", exist_ok=True)
+        os.rename(os.path.join(input_dir, bad_file), os.path.join("bad_files", bad_file))
+        os.rename(os.path.join(output_dir, file), os.path.join("bad_files", file))
+        print(bad_file)
