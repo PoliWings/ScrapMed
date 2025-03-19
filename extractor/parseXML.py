2bd08bf3d2ad9e416a345dc0cdd6878419a9be48
Hunrax <radoslawgajewski03@gmail.com>
2025-03-19 18:55:09 +0100

    remove functionality related to generating metadata.csv file

--- extractor/parseXML.py
+++ extractor/parseXML.py
@@ -1,5 +1,4 @@
 import os
-import csv
 import shutil
 import sys
 import re
@@ -32,7 +31,6 @@ def process_divs(element, ns, stop_event):
                 try:
                     if re.search(r'\b[a-zA-Zą-żĄ-Ż]\w*\b', sentence):
                         langs = detect_langs(sentence)
-                        # print(f"Detected langs: {langs} for sentence: {sentence}")
                         if langs and langs[0].lang == 'pl' and langs[0].prob >= 0.9:
                             text_content += sentence.strip() + " "
                 except Exception as e:
@@ -48,13 +46,6 @@ def process_file(filename, input_folder, output_folder, ns, stop_event):
     try:
         tree = etree.parse(xml_path)
         root = tree.getroot()
-
-        title_element = root.xpath('.//tei:titleStmt/tei:title[@level="a" and @type="main"]', namespaces=ns)
-        title_text = title_element[0].text.strip() if title_element and title_element[0].text else "No Title Found"
-
-        license_element = root.xpath('.//tei:publicationStmt/tei:availability/tei:licence', namespaces=ns)
-        license_text = license_element[0].text.strip() if license_element and license_element[
-            0].text else "No License Information Found"
 
         abstract_element = root.xpath('.//tei:teiHeader/tei:profileDesc/tei:abstract', namespaces=ns)
         abstract_text = ""
@@ -90,7 +81,6 @@ def process_file(filename, input_folder, output_folder, ns, stop_event):
             with open(txt_file_path, 'w', encoding='utf-8') as txtfile:
                 txtfile.write("\n".join(article_content))
             print(f"Processed {filename}")
-            return (base_name, title_text, license_text)
         else:
             print(f"Skipped {filename} because no valid Polish text was found.")
             return None
@@ -99,14 +89,13 @@ def process_file(filename, input_folder, output_folder, ns, stop_event):
         return None
 
 
-def process_xml_files(input_folder, output_folder, csv_file):
+def process_xml_files(input_folder, output_folder):
     ns = {'tei': 'http://www.tei-c.org/ns/1.0'}
     os.makedirs(output_folder, exist_ok=True)
     files = [f for f in os.listdir(input_folder) if f.lower().endswith(".xml")]
 
     stop_event = threading.Event()
 
-    results = []
     detect_langs("Initialize langdetect...")
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=25) as executor:
@@ -115,24 +104,12 @@ def process_xml_files(input_folder, output_folder, csv_file):
 
         try:
             for future in concurrent.futures.as_completed(futures):
-                result = future.result()
-                if result is not None:
-                    results.append(result)
+                future.result()
         except KeyboardInterrupt:
             print("Stopping processing...")
             stop_event.set()
             for future in futures:
                 future.cancel()
-
-    if not os.path.exists(csv_file):
-        with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
-            csv_writer = csv.writer(csvfile)
-            csv_writer.writerow(['filename', 'title', 'url', 'license'])
-
-    with open(csv_file, 'a', newline='', encoding='utf-8') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        for base_name, title_text, license_text in results:
-            csv_writer.writerow([base_name, title_text, "", license_text])
 
     print("Processing completed.")
 
@@ -144,5 +121,4 @@ if __name__ == "__main__":
         if os.path.exists(output_folder):
             print(f"Removing existing folder: {output_folder}")
             shutil.rmtree(output_folder)
-    csv_file = os.path.join(output_folder, "metadata.csv")
-    process_xml_files(input_folder, output_folder, csv_file)
+    process_xml_files(input_folder, output_folder)
